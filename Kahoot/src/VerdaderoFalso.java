cdb7de3830c6c72e9f9891e00888eee2a0eb1624
leandro <leanvanke@live.com.ar>
2020-07-20 12:56:53 -0300

    Falta poner que halla una lista de respuestas

--- Kahoot/src/VerdaderoFalso.java
+++ Kahoot/src/VerdaderoFalso.java
@@ -6,7 +6,7 @@ public class VerdaderoFalso {
     private String contenido;
     private Boolean penalidad;
 
-
+    //Falta que sea una lista de respuestas
     public int recibirRespuesta(Respuesta respuesta){
 
     int puntos = 0;
