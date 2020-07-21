45ca062ecdbd34040ab6932443a40a45ea99aaf2
leandro <leanvanke@live.com.ar>
2020-07-21 01:01:30 -0300

    probando javafx

--- src/main/java/edu/fiuba/algo3/modelo/Panel.java
+++ src/main/java/edu/fiuba/algo3/modelo/Panel.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.modelo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -16,6 +17,15 @@ public class Panel extends Application {
     public void start(Stage stage) {
 
         stage.setTitle("Kahoot");
+
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout);
+        stage.setScene(scene);
+
+        Button button = new Button();
+        button.setText("Boton");
+        layout.getChildren().add(button);
+
         stage.show();
     }
 
