28def5c621d6da584c116ece87823b0550906715
leandro <leanvanke@live.com.ar>
2020-07-20 22:41:26 -0300

    panel

---
+++ Kahoot/src/Panel.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Panel extends Application {
+
+    public static void panel (String[] args){
+
+        launch(args);
+
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
+
