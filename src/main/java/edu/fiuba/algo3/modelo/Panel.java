5c636b349579c2af579e14542e156026a6712f15
leandro <leanvanke@live.com.ar>
2020-07-21 00:55:49 -0300

    solucione un problema con el javaFX que no me andaba

--- src/main/java/edu/fiuba/algo3/modelo/Panel.java
+++ src/main/java/edu/fiuba/algo3/modelo/Panel.java
@@ -2,21 +2,26 @@ package edu.fiuba.algo3.modelo;
 
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
+/**
+ * JavaFX App
+ */
 public class Panel extends Application {
 
-    public static void panel (String[] args){
-
-        launch(args);
-
-    }
-
-
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
+        stage.setTitle("Kahoot");
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
 
