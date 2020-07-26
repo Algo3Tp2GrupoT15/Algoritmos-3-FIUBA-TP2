cf6bbea827cbef7d7a5e4c61d389fc430f93b2f0
leandro <leanvanke@live.com.ar>
2020-07-26 02:52:01 -0300

    agrego al diagrama algunos cambios que hice, faltan algunos

--- src/main/java/edu/fiuba/algo3/modelo/Jugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.modelo;
 
 public class Jugador {
     private int puntaje;
+    private String nombre;
 
     public Jugador() {
         puntaje = 0;
@@ -12,6 +13,11 @@ public class Jugador {
         puntaje = Pregunta.asignarPuntaje(respuesta);
 
 
+    }
+
+    public void asignarNombre(String nombre){
+
+        this.nombre = nombre;
     }
 
     public int puntaje() {
