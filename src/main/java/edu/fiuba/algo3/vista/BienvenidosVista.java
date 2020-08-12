7e40e07a61bbac29725b2e093253a39b8ae1a3ee
leandro <leanvanke@live.com.ar>
2020-08-12 17:26:40 -0300

    agrego bienvenidosVista

---
+++ src/main/java/edu/fiuba/algo3/vista/BienvenidosVista.java
@@ -0,0 +1,51 @@
+package edu.fiuba.algo3.vista;
+
+import edu.fiuba.algo3.vista.controladores.BotonEntrarEventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class BienvenidosVista extends VBox {
+
+
+    private Stage stage;
+
+    public BienvenidosVista(Stage stage, Scene proximaEscena) {
+
+        super();
+
+        this.stage = stage;
+
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(20);
+        this.setPadding(new Insets(25));
+
+        /*Image imagen = new Image("file:src/vista/imagenes/textura.png");
+        BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        this.setBackground(new Background(imagenDeFondo));*/
+
+        Button botonEntrar = new Button();
+        botonEntrar.setText("Entrar");
+
+        Label etiqueta = new Label();
+        etiqueta.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
+
+        etiqueta.setText("Bienvenidos a TP2 KAHOOT de algoritmos y programación III. Haga click en entrar");
+        etiqueta.setTextFill(Color.web("#66A7C5"));
+
+        BotonEntrarEventHandler botonEntrarHandler = new BotonEntrarEventHandler(stage, proximaEscena);
+        botonEntrar.setOnAction(botonEntrarHandler);
+
+        this.getChildren().addAll(etiqueta, botonEntrar);
+    }
+
+
+}
