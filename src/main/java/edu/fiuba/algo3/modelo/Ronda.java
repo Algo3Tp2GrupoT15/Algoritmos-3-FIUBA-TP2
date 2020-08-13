4b73db2bd479a41baf0973ca3ce4c54a87101bdc
Ricarco Luizaga <riluizaga@gmail.com>
2020-08-13 16:18:21 -0300

    Se agrega clase y test ronda

--- src/main/java/edu/fiuba/algo3/modelo/Ronda.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ronda.java
@@ -1,4 +1,39 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.preguntas.Preguntas;
+import edu.fiuba.algo3.modelo.preguntas.VerdaderoYFalso;
+
 public class Ronda {
+    private int ronda;
+    private Preguntas pregunta;
+    private Respuesta respuestaJugador1;
+    private Respuesta respuestaJugador2;
+
+    public Ronda(Jugador jugador1, Jugador jugador2) {
+        respuestaJugador1 = new Respuesta(jugador1);
+        respuestaJugador2 = new Respuesta(jugador2);
+        ronda = 1;
+    }
+
+    public int actual() {
+        return ronda;
+    }
+
+    public void jugar() {
+        pregunta.asignarPuntaje(respuestaJugador1);
+        pregunta.asignarPuntaje(respuestaJugador2);
+        ronda = ronda + 1;
+    }
+
+    public void agregarPregunta(Preguntas verdaderoYFalso) {
+        pregunta = verdaderoYFalso;
+    }
+
+    public void agregarRespuestaJugador1(Respuesta respuestaJugador1) {
+        this.respuestaJugador1 = respuestaJugador1;
+    }
+
+    public void agregarRespuestaJugador2(Respuesta respuestaJugador2) {
+        this.respuestaJugador2 = respuestaJugador2;
+    }
 }
