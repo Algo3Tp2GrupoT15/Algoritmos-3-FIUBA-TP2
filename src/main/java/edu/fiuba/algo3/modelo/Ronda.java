f10ca4325c8c0e9b02ce411d8aa856034d572b49
Ricarco Luizaga <riluizaga@gmail.com>
2020-08-17 01:39:47 -0300

    Se modifica Clase ronda ya que se agrego la clase Turno

--- src/main/java/edu/fiuba/algo3/modelo/Ronda.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ronda.java
@@ -1,38 +1,17 @@
 package edu.fiuba.algo3.modelo;
 
-import edu.fiuba.algo3.modelo.preguntas.Preguntas;
-
 public class Ronda {
-    private int ronda;
-    private Preguntas pregunta;
-    private Respuesta respuestaJugador1;
-    private Respuesta respuestaJugador2;
+    private int numeroDePregunta;
 
-    public Ronda(Jugador jugador1, Jugador jugador2) {
-        respuestaJugador1 = new Respuesta(jugador1);
-        respuestaJugador2 = new Respuesta(jugador2);
-        ronda = 1;
+    public Ronda() {
+        numeroDePregunta = 1;
     }
 
-    public int actual() {
-        return ronda;
+    public int numeroRonda() {
+        return numeroDePregunta;
     }
 
-    public void jugar() {
-        pregunta.asignarPuntaje(respuestaJugador1);
-        pregunta.asignarPuntaje(respuestaJugador2);
-        ronda = ronda + 1;
-    }
-
-    public void agregarPregunta(Preguntas pregunta) {
-        this.pregunta = pregunta;
-    }
-
-    public void agregarRespuestaJugador1(Respuesta respuestaJugador1) {
-        this.respuestaJugador1 = respuestaJugador1;
-    }
-
-    public void agregarRespuestaJugador2(Respuesta respuestaJugador2) {
-        this.respuestaJugador2 = respuestaJugador2;
+    public void siguiente() {
+        numeroDePregunta +=1;
     }
 }
