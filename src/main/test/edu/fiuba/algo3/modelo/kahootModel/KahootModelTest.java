8e0c2282d246373b5db41fe1aa1bc9653fad2d85
Ricarco Luizaga <riluizaga@gmail.com>
2020-08-17 00:36:07 -0300

    Se agrega KahootModelTest

---
+++ src/main/test/edu/fiuba/algo3/modelo/kahootModel/KahootModelTest.java
@@ -0,0 +1,102 @@
+package edu.fiuba.algo3.modelo.kahootModel;
+
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.KahootModel;
+import edu.fiuba.algo3.modelo.Respuesta;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KahootModelTest {
+    @Test
+    public void cargarUnJugadorYPedirElJugadorDeTurnoDevuelveElJugadorTest() {
+        Jugador jugador;
+        KahootModel kahoot = new KahootModel();
+        kahoot.cargarJugador("jugador");
+        jugador = kahoot.jugadorDeTurno();
+        assertEquals("jugador",jugador.nombre());
+    }
+
+    @Test
+    public void cargarDosJugadoresIrAlSiguienteTurnoYPedirElJugadorDeTurnoDevuelveElSegundoJugadorTest() {
+        Jugador jugador;
+        KahootModel kahoot = new KahootModel();
+        kahoot.cargarJugador("jugador1");
+        kahoot.cargarJugador("jugador2");
+        kahoot.siguienteJugador();
+        jugador = kahoot.jugadorDeTurno();
+        assertEquals("jugador2",jugador.nombre());
+    }
+
+    @Test
+    public void proximaPreguntaDeTurnoReiniciaElTurnoPedirElJugadorDeTurnoDevuelveElPrimerJugadorTest() {
+        Jugador jugador;
+        KahootModel kahoot = new KahootModel();
+        kahoot.cargarJugador("jugador1");
+        kahoot.cargarJugador("jugador2");
+        kahoot.siguienteJugador();
+        kahoot.proximaPreguntaDeTurno();
+        jugador = kahoot.jugadorDeTurno();
+        assertEquals("jugador1",jugador.nombre());
+    }
+
+    @Test
+    public void mostrarContenidoDeTurnoTest() {
+        KahootModel kahoot = new KahootModel();
+        kahoot.leerPreguntas();
+        assertEquals(" 2+2=..? ",kahoot.mostrarPreguntaDeTurno());
+    }
+
+    @Test
+    public void proximaPreguntaDeTurnoAvanzaALaSiguientePregunta() {
+        KahootModel kahoot = new KahootModel();
+        kahoot.leerPreguntas();
+        kahoot.proximaPreguntaDeTurno();
+        assertEquals("El cielo es azul",kahoot.mostrarPreguntaDeTurno());
+    }
+
+    @Test
+    public void mostrarOpcionesDeTurnoTest() {
+        KahootModel kahoot = new KahootModel();
+        kahoot.leerPreguntas();
+        assertEquals("4",kahoot.mostrarOpcionesDeTurno().get(0).contenido());
+        assertEquals("2^2",kahoot.mostrarOpcionesDeTurno().get(1).contenido());
+        assertEquals("8",kahoot.mostrarOpcionesDeTurno().get(2).contenido());
+        assertEquals("Pez",kahoot.mostrarOpcionesDeTurno().get(3).contenido());
+    }
+
+    @Test
+    public void alAvanzarConProximaPreguntaDeTurnoAvanzaOpcionesDeTurnoTest() {
+        KahootModel kahoot = new KahootModel();
+        kahoot.leerPreguntas();
+        kahoot.proximaPreguntaDeTurno();
+        assertEquals("verdadero",kahoot.mostrarOpcionesDeTurno().get(0).contenido());
+        assertEquals("falso",kahoot.mostrarOpcionesDeTurno().get(1).contenido());
+    }
+
+    @Test
+    void cargarRespuestasDelTurnoTest() {
+        KahootModel kahoot = new KahootModel();
+        kahoot.cargarJugador("jugador1");
+        kahoot.cargarJugador("jugador2");
+        kahoot.leerPreguntas();
+
+        Respuesta respuestaJugador1 = new Respuesta(kahoot.jugadorDeTurno());
+        respuestaJugador1.agregarOpcion(kahoot.mostrarOpcionesDeTurno().get(2));
+        respuestaJugador1.agregarOpcion(kahoot.mostrarOpcionesDeTurno().get(3));
+        kahoot.cargarRespuestas(respuestaJugador1);
+
+        kahoot.calcularPuntosDelTurno();
+        assertEquals(0,kahoot.jugadorDeTurno().puntaje());
+
+        kahoot.siguienteJugador();
+
+        Respuesta respuestaJugador2 = new Respuesta(kahoot.jugadorDeTurno());
+        respuestaJugador2.agregarOpcion(kahoot.mostrarOpcionesDeTurno().get(0));
+        respuestaJugador2.agregarOpcion(kahoot.mostrarOpcionesDeTurno().get(1));
+        kahoot.cargarRespuestas(respuestaJugador2);
+
+        kahoot.calcularPuntosDelTurno();
+        assertEquals(1,kahoot.jugadorDeTurno().puntaje());
+    }
+}
